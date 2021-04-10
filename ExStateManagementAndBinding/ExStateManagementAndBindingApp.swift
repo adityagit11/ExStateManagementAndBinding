910ca22a6e440b2c4fa7e4ccc0057b1d3fd1a77b
Aditya Git <adityasinghwolvieait@gmail.com>
2021-04-10 16:20:36 +0530

    Initial Commit

---
+++ ExStateManagementAndBinding/ExStateManagementAndBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExStateManagementAndBindingApp.swift
+//  ExStateManagementAndBinding
+//
+//  Created by Aditya Singh on 10/04/21.
+//
+
+import SwiftUI
+
+@main
+struct ExStateManagementAndBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
